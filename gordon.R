85f7a6294c3025e1feaa0a56fde49572de4863e4
Guilhem Piat <guilhempiat@yahoo.fr>
2018-01-03 04:12:40 +0100

    Harmonized code

--- gordon.R
+++ gordon.R
@@ -1,3 +1,4 @@
+#### preprocessing ####
 #load gordon
 gordon <- read.csv("C:/Users/Syncrossus/Documents/Projet_Visualisation/gordon-2002_database.txt", header = TRUE, sep = "\t")
 #getting an idea of what gordon is like
@@ -11,199 +12,62 @@ gordon <- t(gordon)
 gordon.labels <- as.factor(gordon[2:nrow(gordon),1])
 colnames(gordon)<- gordon[1,]
 gordon <- gordon[2:nrow(gordon),2:ncol(gordon)]
-View(gordon[1:10, 1:10])
-
-#Getting better understanding of gordon
-summary(gordon)
-boxplot(gordon[,sample(1:ncol(gordon), 5)])
-gordon[,1]
-
-# the numeric values seem to not be numeric
-# I haven't found a smarter way to convert all cells to numeric without losing column names, row names, or 2D structure.
+# the numeric values are strings and need to be parsed
 gordon <- type.convert(x = gordon, dec = '.')
-View(gordon[1:10, 1:10])
 
-#Trying again
-summary(gordon)
-boxplot(gordon[,sample(1:ncol(gordon), 5)])
-gordon[,1]
 
+#### raw visualization ####
 # Given that the variables describe gene expression, extracting features doesn't make much sense : we want to know which genes are most correlated to a given illness.
 # Therefore, we will select the features most correlated to the labels
 
 # However, let us plot the 2 first dimensions of the PCA just to get an idea
 gordon.pca <- prcomp(gordon)
-plot(gordon.pca$x[, 1:2], col=as.numeric(gordon.labels))
+plot(gordon.pca$x[, 1:2], col=as.numeric(gordon.labels), xlab="Gordon PCA dimension 1", ylab="Gordon PCA dimension 2")
 # The classes are mostly seperate. However, the separation is not linear in the two first dimensions of the PCA and there is some overlap between classes
 
+#LDA
+library(MASS)
+# As we have the classes of the data points, we can try an LDA
+gordon.lda <- lda(formula=gordon.labels~., data = as.data.frame(gordon))
+plot(gordon.lda)
 
-# Variable selection
+#MDS
+gordon.mds <- isoMDS(dist(gordon), k=2, trace = TRUE)
+plot(gordon.mds$points, col=as.numeric(as.factor(gordon.labels)), xlab="Gordon MDS dimension 1", ylab="Gordon MDS dimension 2")
+# Same as PCA
+
+# isomap
+library(vegan)
+gordon.isomap <- isomap(dist = dist(gordon), ndim = 2, k=10, fragmentedOK = TRUE)
+plot(gordon.isomap, col=as.numeric(gordon.labels), xlab="Gordon ISOMAP dimension 1", ylab="Gordon ISOMAP dimension 2")
+
+
+#### Variable selection ####
 # computing correlation of all variables with class
 gordon.cor <- cor(gordon, as.numeric(gordon.labels))
 # ordering dimensions as a function of their correlation to the labels
 gordon.ordered <- gordon[,rev(order(abs(gordon.cor)))]
+# After some experimentation, about 75 variables seemed optimal
+gordon.select <- gordon.ordered[, 1:75]
 
-# viewing 10 variables most correlated with class
-gordon.ordered[1:10,1:10]
-
-# We can probably assume we will have enough of 100 dimensions, especially given how few observations we have for the number of dimensions
-gordon.select <- gordon.ordered[, 1:100]
-# let's view the first dimensions
-plot(gordon.select[,1:2], col=as.numeric(gordon.labels))
-# separability is better than with PCA, and the density of the classes in these dimensions is very different.
-
+#### final visualization ####
 # let us view the data by reducing the dimensions by MDS
-#MDS
-library(MASS)
-gordon.mds <- isoMDS(dist(gordon.select), k=2, trace = TRUE)
-plot(gordon.mds$points, col=as.numeric(as.factor(gordon.labels)))
+#PCA
+gordon.sel.pca <- prcomp(gordon.select)
+plot(gordon.sel.pca$x[, 1:2], col=as.numeric(gordon.labels), xlab="Gordon post selection PCA dimension 1", ylab="Gordon post selection PCA dimension 2")
 # the data is linearly separable.
 
+#MDS
+gordon.sel.mds <- isoMDS(dist(gordon.select), k=2, trace = TRUE)
+plot(gordon.sel.mds$points, col=as.numeric(as.factor(gordon.labels)), xlab="Gordon post selection MDS dimension 1", ylab="Gordon post selection MDS dimension 2")
+# same results as PCA
+
 # As we have the classes of the data points, we can try an LDA
-gordon.lda <- lda(formula=gordon.labels~., data = as.data.frame(gordon.select))
-plot(gordon.lda)
-# The LDA reduces the space to 1 dimension with the datapoints linearly separable.
+gordon.sel.lda <- lda(formula=gordon.labels~., data = as.data.frame(gordon.select))
+plot(gordon.sel.lda)
+# The LDA reduces the space to 1 dimension with linearly separable classes.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # removing redundant dimensions
-# gordon.select <- gordon.ordered
-# for (i in 1:10) {
-#   gordon.select <- gordon.select[,-which(abs(cor(gordon.select)[i,])>0.60)]
-# }
-# gordon.select10 <- gordon.select[,1:10]
-# 
-# 
-# 
-# #PCA
-# gordon.pca <- prcomp(gordon)
-# plot(gordon.pca$sdev)
-# plot(gordon.pca$x[, 1:2], col=as.numeric(as.factor(gordon.labels)))
-# 
-# #LDA
-# library(MASS)
-# gordon.lda <- lda(formula=gordon.labels~., data = as.data.frame(gordon))
-# plot(gordon.lda, col = as.numeric(as.factor(gordon.labels)))
-# plot(x=gordon.lda$scaling, y = as.numeric(as.factor(gordon.labels)))
-# View(gordon.lda$scaling)
-# 
-# #MDS
-# library(MASS)
-# gordon.mds <- isoMDS(dist(gordon.ordered[,1:100]), k=2)
-# plot(gordon.mds$points, col=as.numeric(as.factor(gordon.labels)))
-# 
-# #SOM
-# library(kohonen)
-# gordon.som <- som(X = gordon, grid = somgrid(xdim = 12, ydim=12, topo="hexagonal"), rlen=200)
-# plot(gordon.som, type="changes")
-# plot(gordon.som, type="count")
-# plot(gordon.som, type="dist.neighbours")
-# 
-# 
-# 
-# # plotting
-# plot(gordon.select10[,1:2], col=as.numeric(as.factor(gordon.labels)))
-# pairs(gordon.select10[,1:4], col=as.numeric(as.factor(gordon.labels)))
-# 
-# #LDA
-# library(MASS)
-# nMPM <- sum(as.numeric(as.factor(gordon.labels))-1)
-# gordon.s10.lda <- lda(formula = as.numeric(as.factor(gordon.labels))~., data = data.frame(gordon.ordered[,1:100]))
-# gordon.s10.lda$terms
-# plot(gordon.s10.lda)
-# #
-# 
-# 
-# #PCA
-# library(FactoMineR)
-# gordon.s10.pca <- PCA(gordon.ordered[,1:100], scale.unit = TRUE)
-# plot.PCA(gordon.s10.pca)
-# plot(gordon.s10.pca$ind$coord, col = as.numeric(as.factor(gordon.labels)))
-# #
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# ##### doesn't work
-# gordon.chisq <- t(scale(t(gordon)))
-# gordon.chisq.cor <- cor(gordon.chisq, as.numeric(as.factor(gordon.labels)))
-# gordon.chisq.select10 <- gordon.chisq[,rev(order(abs(gordon.chisq.cor)))[1:10]]
-# plot(gordon.chisq.select10[,2:3], col=as.numeric(as.factor(gordon.labels)))
-# pairs(gordon.chisq.select10[,1:5], col=as.numeric(as.factor(gordon.labels)))
-# 
-# 
-# 
-# gordon.AD <- gordon[which(gordon.labels=="AD"),]
-# gordon.MPM <- gordon[which(gordon.labels=="MPM"),]
-# 
-# gordon.normalized <- rbind(t(scale(t(gordon.AD), center=FALSE)),t(scale(t(gordon.MPM), center=FALSE)))
-# gordon.norm.cor <- cor(gordon.normalized, as.numeric(as.factor(gordon.labels)))
-# gordon.norm.select10 <- gordon.normalized[, rev(order(abs(gordon.norm.cor)))[1:10]]
-# plot(gordon.norm.select10[,1:2], col=as.numeric(as.factor(gordon.labels)))
-# pairs(gordon.norm.select10[,1:5], col=as.numeric(as.factor(gordon.labels)))
-# 
-# 
-# 
-# 
-# a.scaled<-scale(t(gordon.cor))
-# a.scaled<-t(a.scaled)
-# gordon.cor + attr(a.scaled, "scaled:center")
-# #
-# 
-# 
-# for(i in 1:nrow(gordon.chisq)){
-#   gordon.chisq[1,]<-gordon.chisq[1,]/sd(gordon.chisq[1,])
-# }
-# gordon.cor = matrix(1:4, 2)
-# 
-# apply(x = gordon.cor, MARGIN = 1, FUN = shaz)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# isomap
+gordon.isomap <- isomap(dist = dist(gordon.select), ndim = 2, k=10, fragmentedOK = TRUE)
+plot(gordon.isomap, col=as.numeric(gordon.labels), xlab="Gordon post selection ISOMAP dimension 1", ylab="Gordon post selection ISOMAP dimension 2")
+# the data is linearly separable, but no advantage compared to PCA
--- pomeroy.R
+++ pomeroy.R
@@ -1,44 +1,69 @@
+#### preprocessing ####
 #load pomeroy
-pomeroy <- read.csv("C:/Users/Syncrossus/Documents/GitHub/gordon-pomeroy-visualization/pomeroy-2002-v2_database.txt", header = TRUE, sep = "\t")
-#getting an idea of what pomeroy is like
-View(pomeroy[1:10, 1:10])
+pomeroy <- read.csv("pomeroy.csv",stringsAsFactors=FALSE, header = TRUE)
+#overview
+View(as.data.frame(pomeroy[1:10, 1:10]))
 which(is.na(pomeroy))
 dim(pomeroy)
 levels(pomeroy[,2])
 
-#strangely, observations seem to be columns and variables rows... also, the row labels are in the first column
+#preprocessing
 pomeroy <- t(pomeroy)
-pomeroy.labels <- pomeroy[2:nrow(pomeroy),1]
+pomeroy.labels <- as.factor(pomeroy[2:nrow(pomeroy),1])
 colnames(pomeroy)<- pomeroy[1,]
 pomeroy <- pomeroy[2:nrow(pomeroy),2:ncol(pomeroy)]
-View(pomeroy[1:10, 1:10])
-
-#Trying to get a better understanding of pomeroy
-summary(pomeroy)
-boxplot(pomeroy[,sample(1:ncol(pomeroy), 5)])
-pomeroy[,1]
-
-# the numeric values seem to not be numeric
-# I haven't found a smarter way to convert all cells to numeric without losing column names, row names, or 2D structure.
 pomeroy <- type.convert(x = pomeroy, dec = '.')
-View(pomeroy[1:10, 1:10])
 
-#Trying again
-summary(pomeroy)
-boxplot(pomeroy[,sample(1:ncol(pomeroy), 5)])
-pomeroy[,1]
-
-
-
+#### raw visualization ####
 #PCA
-pomeroy.pca <- prcomp(pomeroy)
-plot(pomeroy.pca$sdev)
-plot(pomeroy.pca$x[, 1:2], col=as.numeric(as.factor(pomeroy.labels)))
+library(rgl)
+pomeroy.pca = prcomp(pomeroy)
+plot(pomeroy.pca$x[,1:2],col= as.integer(pomeroy.labels), xlab="Pomeroy PCA dimension 1", ylab="Pomeroy PCA dimension 2")
+plot3d(pomeroy.pca$x[,1:3],col= as.integer(pomeroy.labels) ,legend=TRUE, image=TRUE, size = 8,cex.legend = 1)
 
 #LDA
 library(MASS)
 pomeroy.lda <- lda(formula=pomeroy.labels~., data = as.data.frame(pomeroy))
-plot(pomeroy.lda, col = as.numeric(as.factor(pomeroy.labels)))
-View(pomeroy.lda$scaling)
+plot(pomeroy.lda, col=as.numeric(pomeroy.labels))
+
+#MDS
+pomeroy.mds <- isoMDS(dist(pomeroy), k=10, trace = TRUE)
+plot(pomeroy.mds$points, col=as.numeric(as.factor(pomeroy.labels)))
+#same as PCA
+
+#Isomap
+library(vegan)
+pomeroy.isomap <- isomap(dist = dist(pomeroy), ndim = 2, k=10, fragmentedOK = TRUE)
+plot(pomeroy.isomap, col=as.numeric(pomeroy.labels), xlab="Pomeroy ISOMAP dimension 1", ylab="Pomeroy ISOMAP dimension 2")
 
 
+#### Variable selection ####
+# computing correlation of all variables with class
+pomeroy.cor <- cor(pomeroy, as.numeric(pomeroy.labels))
+# ordering dimensions as a function of their correlation to the labels
+pomeroy.ordered <- pomeroy[,rev(order(abs(pomeroy.cor)))]
+# After some experimentation, about 75 variables seemed optimal
+pomeroy.select <- pomeroy.ordered[, 1:75]
+
+#### final visualization ####
+#PCA
+pom.sel.pca <- prcomp(pomeroy.select)
+plot(pom.sel.pca$x[,1:2],col= as.numeric(pomeroy.labels))
+# No identifiable pattern in data
+
+#LDA
+pom.sel.lda <- lda(formula=pomeroy.labels~., data = as.data.frame(pomeroy.select))
+plot(pom.sel.lda, col= as.numeric(pomeroy.labels))
+#each class is linearly separable from the others
+
+#MDS
+library(MASS)
+pom.sel.mds <- isoMDS(dist(pomeroy.select), k=2, trace = TRUE)
+plot(pom.sel.mds$points, col= as.numeric(pomeroy.labels))
+# Same as PCA
+
+#Isomap
+library(vegan)
+pom.sel.isomap <- isomap(dist = dist(pomeroy.select), ndim = 2, k=10, fragmentedOK = TRUE)
+plot(pom.sel.isomap, col=as.numeric(pomeroy.labels), xlab="Pomeroy ISOMAP dimension 1", ylab="Pomeroy ISOMAP dimension 2")
+# No identifiable pattern in data
